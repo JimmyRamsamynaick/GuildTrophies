9aa5133a7fa545aeb3fa3c96639531f1d8ef247f
JimmyRamsamynaick <jimmyramsamynaick@gmail.com>
2025-12-25 17:01:25 +0100

    Emojis: utilise les IDs fournis (<:trophybronze/...>) comme valeurs par défaut

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ module.exports = {
   clientId: process.env.CLIENT_ID || '',
   dbPath: './trophies.db',
   rarityEmojis: {
-    BRONZE: process.env.BRONZE_EMOJI || '🥉',
-    SILVER: process.env.SILVER_EMOJI || '🥈',
-    GOLD: process.env.GOLD_EMOJI || '🥇',
-    PLATINUM: process.env.PLATINUM_EMOJI || '💎'
+    BRONZE: process.env.BRONZE_EMOJI || '<:trophybronze:1453779318936440863>',
+    SILVER: process.env.SILVER_EMOJI || '<:trophyargent:1453779320316104786>',
+    GOLD: process.env.GOLD_EMOJI || '<:trophyor:1453779321666666690>',
+    PLATINUM: process.env.PLATINUM_EMOJI || '<:trophyplatine:1453779322530828461>'
   }
 };
